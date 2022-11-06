bf956fc6ff094112a8822f336c40e4429df3e908
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2022-11-06 15:04:08 +0100

    config: add additional ssh key

--- modules/perf-test-01.nix
+++ modules/perf-test-01.nix
@@ -5,13 +5,14 @@
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   networking.hostName = "perf-test-01";
-  
+
   boot.cleanTmpDir = true;
   zramSwap.enable = true;
   services.openssh.enable = true;
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErZm6k0S7NahikKEbTQlrOrsLKgr9X+iNoUsGeqDV0F julian@canaan.xn--pl-wia.net" 
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErZm6k0S7NahikKEbTQlrOrsLKgr9X+iNoUsGeqDV0F julian@canaan.xn--pl-wia.net"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICeIiTyh7jJD9x8N64kgUGDgeo3F96i5Av3tHvwePHq5 julian@babylon"
   ];
 
   boot.loader.grub.device = "/dev/sda";
